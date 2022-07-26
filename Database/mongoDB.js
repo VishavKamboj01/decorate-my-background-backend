c548f4a818a48d55aa4e800a10cbe8fccbc31754
VishavKamboj01 <vishavbehar@gmail.com>
2022-07-26 12:14:45 +0530

    Added database string to confit module

--- Database/mongoDB.js
+++ Database/mongoDB.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
-import json from "../config/default.json" assert { type: "json" };
+import config from "config";
 
 function connectToDB() {
   mongoose
-    .connect(json.db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(config.get("db"), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then(() => console.log("Connected to DataBase..."))
     .catch((err) =>
       console.error("Could not connect to database... error ", err)
