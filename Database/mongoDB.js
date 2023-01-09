3e79c466b753a0b044b9d62e6d06c5fa6bb40fba
VishavKamboj01 <vishavbehar@gmail.com>
2023-01-09 20:22:03 +0530

    Changed mongoDB connection file

--- Database/mongoDB.js
+++ Database/mongoDB.js
@@ -6,8 +6,8 @@ function connectToDB() {
   const url = config.get("db");
   mongoose
     .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => winston.info("Connected to DataBase... " + url))
-    .catch((err) => winston.error("Could not connect to DB " + err + url));
+    .then(() => console.log("Connected to DataBase... " + url))
+    .catch((err) => console.log("Could not connect to DB " + err+" "+ url));
 }
 
 export { connectToDB };
